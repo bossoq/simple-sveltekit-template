ea4dcc943dbd6bbde4bd22565dcbf4a12130a38a
Kittipos Wajakajornrit <boss.zz@me.com>
2022-03-01 13:54:11 +0700

    Add line share button

--- src/lib/components/Social.svelte
+++ src/lib/components/Social.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Facebook, Twitter } from 'svelte-share-buttons-component'
+  import { Facebook, Twitter, Line } from 'svelte-share-buttons-component'
 
   export let url: string
   export let title: string
@@ -9,7 +9,8 @@
   <div
     class="w-full fixed top-auto bottom-4 center flex gap-2 pr-4 sm:pr-0 justify-end sm:justify-center"
   >
-    <Facebook class="h-10 w-10 rounded-full" {url} text={title} />
+    <Facebook class="h-10 w-10 rounded-full" {url} quote={title} />
     <Twitter class="h-10 w-10 rounded-full" {url} text={title} />
+    <Line class="h-10 w-10 rounded-full" {url} />
   </div>
 {/if}
